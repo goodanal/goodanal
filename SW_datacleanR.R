3c9748a3f87a94ec44c2bb9fd06b9ca01be6b78d
goodanal <118525378+goodanal@users.noreply.github.com>
2022-11-18 06:05:56 -0500

    Add files via upload

---
+++ SW_datacleanR.R
@@ -0,0 +1,20 @@
+summary(X13_minuteMETsNarrow_merged_minuteMETsNarrow_merged)
+
+summary(X17_sleepDay_merged_csv_Copy_of_sleepDay)
+head(X17_sleepDay_merged_csv_Copy_of_sleepDay)
+X17_sleepDay_merged_csv_Copy_of_sleepDay %>% Group_By (TotalSleepRecords)
+install.packages("here")
+library("here")
+install.packages("skimr")
+library("skimr")
+install.packages("janitor")
+library("janitor")
+install.packages("dlpyr")
+library("dplyr")
+skim_without_charts(X01_dailyActivity_merged_csv_Copy_of_dailyActivity_merged)
+glimpse((X02_dailyCalories_merged_csv_dailyCalories))
+
+
+head(X01)
+head(X01_dailyActivity_merged_csv_Copy_of_dailyActivity_merged)
+separate(X05_heartrate_seconds_merged_heartrate_seconds_merged,Time,into=c('date','time'), sep=' ')
